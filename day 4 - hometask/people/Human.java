d18b0571b285b11a6f41fe0d3d7fbe66a1c11f32
Kate Sychova <sychovakate@mail.ru>
2020-04-09 23:21:31 +0300

    Changed Java files

---
+++ day 4 - hometask/people/Human.java
@@ -0,0 +1,17 @@
+package com.DAY_4.people;
+
+public class Human {
+    int age;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void canSpeak(){
+        System.out.println("I'm a man! Of course I can talk.");
+    }
+}
---
+++ day 4 - hometask/people/Person.java
@@ -0,0 +1,22 @@
+package com.DAY_4.people;
+
+public class Person extends Human {
+    String name;
+    String surname;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+}
